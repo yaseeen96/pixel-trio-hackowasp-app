6d0de2001e1dc05609c77d1fbf8874ef7e155159
yaseen5748 <syedyaseen1539@gmail.com>
2023-04-16 09:01:44 +0530

    add dummy data in myPrints section

--- screens/AfterLoginScreens/myPrints.js
+++ screens/AfterLoginScreens/myPrints.js
@@ -1,8 +1,14 @@
 import { useTheme } from "@react-navigation/native";
 import React, { useEffect } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-import { TextInput } from "react-native-paper";
+import {
+  TextInput,
+  Searchbar,
+  Card,
+  Button,
+  IconButton,
+} from "react-native-paper";
 
 const MyPrintScreen = ({ navigation }) => {
   const { colors } = useTheme();
@@ -10,7 +16,31 @@ const MyPrintScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My Prints screen</Text>
+      <Text style={styles.heading}>My Prints</Text>
+      <Searchbar style={styles.searchBar} placeholder="Search by filename" />
+      <Card style={styles.card}>
+        <View style={{ flexDirection: "row" }}>
+          <Card.Cover
+            source={{ uri: "item.vendor.image" }}
+            style={{ width: "40%", height: "100%" }}
+          />
+          <View style={styles.cardContent}>
+            <Card.Title
+              title={"Sample.pdf"}
+              titleStyle={{ color: colors.text, fontWeight: "bold" }}
+            />
+
+            <Card.Content>
+              <Text style={styles.labelText}>16-04-23</Text>
+            </Card.Content>
+
+            <Card.Actions>
+              <Button>Open</Button>
+              <IconButton icon="share" color={colors.primary} size={20} />
+            </Card.Actions>
+          </View>
+        </View>
+      </Card>
     </View>
   );
 };
@@ -21,11 +51,23 @@ const getStyles = ({ colors }) =>
       display: "flex",
       flex: 1,
       backgroundColor: colors.background,
-      justifyContent: "center",
-      alignItems: "center",
+      padding: "8%",
     },
-    text: {
+    heading: {
       color: colors.text,
+      fontSize: 40,
+      fontWeight: "bold",
+    },
+    card: {
+      width: "100%",
+      marginVertical: "10%",
+    },
+    searchBar: {
+      marginTop: "10%",
+    },
+    cardContent: {
+      width: "60%",
+      padding: "2%",
     },
   });
 
